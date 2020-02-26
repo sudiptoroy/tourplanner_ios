6d422a4eb017627e84def65a75a21427f1522f1a
Farisa <macFarisa@labs-mac.local>
2020-02-26 17:28:24 +0600

    Initial Commit

---
+++ TourPlanner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TourPlanner
+//
+//  Created by Farisa on 2/26/20.
+//  Copyright © 2020 Code_x. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
