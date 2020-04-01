5560e79ce2f552662b548bb4cdd9259b09047e90
Sudipto Roy <sudiptoroy.cse@gmail.com>
2020-04-01 19:27:45 +0600

    Parse json to display in Home page

--- TourPlanner/Controller/Tab Bar Controller/HomeViewController.swift
+++ TourPlanner/Controller/Tab Bar Controller/HomeViewController.swift
@@ -8,13 +8,48 @@
 
 import Foundation
 import UIKit
+import Alamofire
 
 class HomeViewController: UIViewController {
+    
+    @IBOutlet weak var ratingLabel: UILabel!
     
     var id: Int?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         print(id as Any)
+        getGuideProfile()
+    }
+    
+    
+    func getGuideProfile() {
+        
+        let param = ["id" : id!,
+                     "api_key": API.API_key] as [String: Any]
+        
+        Alamofire.request(API.baseURL + "/guides/ProfileSelf", method: .post, parameters: param).validate().responseJSON {
+            response in
+            
+            print(response)
+            
+            // let loginResponseData = try JSONDecoder().decode(Guide.self, from: response.data!)
+            do {
+                
+                let guideProfileResponse = try JSONDecoder().decode(GuideProfile.self, from: response.data!)
+                let rating = guideProfileResponse.data[0].ratings
+                print("Rating")
+                print(rating!)
+                if rating == 0 {
+                    self.ratingLabel.text = "N/A"
+                } else {
+                    self.ratingLabel.text = "\(String(describing: rating))"
+                    //self.ratingLabel.text = rating as? String
+                }
+                
+            } catch {
+                print("Error While parsing")
+            }
+        }
     }
 }
