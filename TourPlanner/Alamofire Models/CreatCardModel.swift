9091220d54d3770d1f630ca448c774ed04c92f45
Sudipto Roy <sudiptoroy.cse@gmail.com>
2020-04-02 12:33:28 +0600

    Modify CreateCardModel

--- TourPlanner/Alamofire Models/CreatCardModel.swift
+++ TourPlanner/Alamofire Models/CreatCardModel.swift
@@ -13,22 +13,5 @@ struct CreateCard: Decodable {
     
     let success: Bool
     let message: String
-    let data: [Card]
 }
 
-
-struct Card: Decodable {
-    
-    let guide_id: Int?
-    let card_title: String?
-    let card_description: String?
-    let price_per_day: Int?
-    let place_ids: String?
-    let service_status: Int?
-    let card_status: Int?
-    let card_category_tags: String?
-    let updated_at: String?
-    let created_at: String?
-    let id: Int?
-    
-}
