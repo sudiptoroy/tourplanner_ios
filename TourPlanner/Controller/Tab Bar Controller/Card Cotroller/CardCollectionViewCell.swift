e3998cb83bd8d18c7e5c0069f6cb2c88900485c3
Sudipto Roy <sudiptoroy.cse@gmail.com>
2020-04-02 18:04:20 +0600

    Show Cards with collection view (In Progress)

---
+++ TourPlanner/Controller/Tab Bar Controller/Card Cotroller/CardCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  CardCollectionViewCell.swift
+//  TourPlanner
+//
+//  Created by Sudipto Roy on 4/2/20.
+//  Copyright © 2020 Code_x. All rights reserved.
+//
+
+import UIKit
+
+class CardCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var cardTitle: UILabel!
+    @IBOutlet weak var cardRating: UILabel!
+    @IBOutlet weak var pricePerDay: UILabel!
+    
+}
--- TourPlanner/Controller/Tab Bar Controller/Card Cotroller/ShowCardsViewController.swift
+++ TourPlanner/Controller/Tab Bar Controller/Card Cotroller/ShowCardsViewController.swift
@@ -7,15 +7,19 @@
 //
 
 import UIKit
+import Alamofire
 
-class ShowCardsViewController: UIViewController {
+class ShowCardsViewController: UICollectionViewController {
     
+    @IBOutlet weak var CardCollectionView: UICollectionView!
     var id: Int?
+    var data = [[String: Any]]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         print ("Id in show card")
         print(id as Any)
+        getCardsByGuideID()
         // Do any additional setup after loading the view.
     }
     
@@ -23,6 +27,33 @@ class ShowCardsViewController: UIViewController {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if let createCardViewController = segue.destination as? CreateCardViewController {
             createCardViewController.id = id
+        }
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! CardCollectionViewCell
+        return cell
+    }
+    
+    func getCardsByGuideID () {
+        let param = ["id": id as Any,
+                     "api_key": API.API_key]
+        
+        Alamofire.request(API.baseURL + "/cards/ByGuideID", method: .post, parameters: param).validate().responseJSON {
+            response in
+            
+            do {
+                //let createCardResponse = try JSONDecoder().decode(CreateCard.self, from: response.data!)
+                let getCardByGuideResponse = try JSONDecoder().decode(CardByGuide.self, from: response.data!)
+                print(getCardByGuideResponse)
+                
+            } catch {
+                print("Error While parsing Json")
+            }
         }
     }
 
