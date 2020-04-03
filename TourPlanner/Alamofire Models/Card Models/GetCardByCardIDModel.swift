3c13aa353ac8d5624c07aa4d03396ce7e42e3ac1
Sudipto Roy <sudiptoroy.cse@gmail.com>
2020-04-03 15:13:10 +0600

    Add CardByCardID json parsing model

---
+++ TourPlanner/Alamofire Models/Card Models/GetCardByCardIDModel.swift
@@ -0,0 +1,28 @@
+//
+//  GetCardByCardIDModel.swift
+//  TourPlanner
+//
+//  Created by Sudipto Roy on 4/3/20.
+//  Copyright © 2020 Code_x. All rights reserved.
+//
+
+import Foundation
+
+
+struct CardByID: Decodable {
+    let success: Bool
+    let data: [CardByIDDetails]
+}
+
+struct CardByIDDetails: Decodable {
+    let id: Int?
+    let card_title: String?
+    let card_description: String?
+    let price_per_hour: Int?
+    let price_per_day: Int?
+    let place_ids: String?
+    let card_average_rating: Double?
+    let service_status: Int?
+    let card_category_tags: String?
+    
+}
