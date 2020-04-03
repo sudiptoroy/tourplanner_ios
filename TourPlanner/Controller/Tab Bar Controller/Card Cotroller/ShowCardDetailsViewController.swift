224d59a4b47c92080e414d83e586d7bc2e98168b
Sudipto Roy <sudiptoroy.cse@gmail.com>
2020-04-03 15:31:11 +0600

    Add feature of Show Card details

--- TourPlanner/Controller/Tab Bar Controller/Card Cotroller/ShowCardDetailsViewController.swift
+++ TourPlanner/Controller/Tab Bar Controller/Card Cotroller/ShowCardDetailsViewController.swift
@@ -7,8 +7,16 @@
 //
 
 import UIKit
+import Alamofire
 
 class ShowCardDetailsViewController: UIViewController {
+    
+    @IBOutlet weak var cardImage: UIImageView!
+    @IBOutlet weak var cardTitleLabel: UILabel!
+    @IBOutlet weak var cardRatingLabel: UILabel!
+    @IBOutlet weak var cardDetailsTextView: UITextView!
+    @IBOutlet weak var cardPricePerDayLabel: UILabel!
+    
     
     var cardIDReceived = ""
 
@@ -16,19 +24,40 @@ class ShowCardDetailsViewController: UIViewController {
         super.viewDidLoad()
         print("Card Id recieved: ")
         print(cardIDReceived)
-
+        self.showCardDetails()
         // Do any additional setup after loading the view.
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func showCardDetails () {
+        let cardIDConverted = Int(cardIDReceived)
+        
+        let param = ["id" : cardIDConverted!,
+                     "api_key" : API.API_key] as [String: Any]
+        
+        Alamofire.request(API.baseURL + "/cards/ByID", method: .post, parameters: param).validate().responseJSON {
+            response in
+            
+            if ((response.result.value) != nil) {
+                do {
+                    let getCardByCardIDResponse = try JSONDecoder().decode(CardByID.self, from: response.data!)
+                    let cardTitle = getCardByCardIDResponse.data[0].card_title
+                    let cardDescription = getCardByCardIDResponse.data[0].card_description
+                    let cardRating = getCardByCardIDResponse.data[0].card_average_rating
+                    let cardPricePerDay = getCardByCardIDResponse.data[0].price_per_day
+                    
+                    //print(getCardByCardIDResponse)
+                    
+                    // Show in the UI
+                    self.cardTitleLabel.text = cardTitle
+                    self.cardDetailsTextView.text = cardDescription
+                    self.cardPricePerDayLabel.text = "$" + String(cardPricePerDay!) + "/Day"
+                    self.cardRatingLabel.text = String(cardRating ?? 0)
+                    
+                } catch {
+                    print("Error while parsing Json for GetCardByCardID")
+                }
+            }
+        }
     }
-    */
-
 }
