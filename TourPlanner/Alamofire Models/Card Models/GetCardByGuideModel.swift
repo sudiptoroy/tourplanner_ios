f6375f8461664a79f4dd131c55c6c1d3eeabb1a4
Sudipto Roy <sudiptoroy.cse@gmail.com>
2020-04-03 11:49:15 +0600

    Modify GetCardByGuideModel for json parsing error

--- TourPlanner/Alamofire Models/Card Models/GetCardByGuideModel.swift
+++ TourPlanner/Alamofire Models/Card Models/GetCardByGuideModel.swift
@@ -21,11 +21,26 @@ struct Card: Decodable {
     let guide_id: Int?
     let card_description: String?
     let price_per_hour: Int?
+    let price_per_day: Int?
     let place_ids: String?
-    let card_average_rating: Int?
+    let card_average_rating: Double?
     let service_status: Int?
     let card_status: Int?
     let card_category_tags: String?
     let created_at: String?
     let updated_at: String?
 }
+
+//"id": 3,
+//"card_title": "I'll help to visit Dhaka",
+//"guide_id": 8,
+//"card_description": "Dhaka is the capital of Bangladesh",
+//"price_per_hour": 5,
+//"price_per_day": 30,
+//"place_ids": "1,2,3",
+//"card_average_rating": null,
+//"service_status": 0,
+//"card_status": 1,
+//"card_category_tags": "Dhaka",
+//"created_at": "2020-04-01 17:27:12",
+//"updated_at": "2020-04-01 17:27:12"
