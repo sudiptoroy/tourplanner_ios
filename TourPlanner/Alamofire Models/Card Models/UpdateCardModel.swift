719c3fc50a64328595ebbe6ae41d7495511d441c
Sudipto Roy <sudiptoroy.cse@gmail.com>
2020-04-04 16:28:37 +0600

    Add Card Update Feature

---
+++ TourPlanner/Alamofire Models/Card Models/UpdateCardModel.swift
@@ -0,0 +1,14 @@
+//
+//  UpdateCardModel.swift
+//  TourPlanner
+//
+//  Created by Sudipto Roy on 4/4/20.
+//  Copyright © 2020 Code_x. All rights reserved.
+//
+
+import Foundation
+
+struct UpdateCard: Decodable {
+    let success: Bool
+    let message: String
+}
--- TourPlanner/Controller/Tab Bar Controller/Card Cotroller/EditCardViewController.swift
+++ TourPlanner/Controller/Tab Bar Controller/Card Cotroller/EditCardViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Alamofire
 
 class EditCardViewController: UIViewController {
 
@@ -28,6 +29,7 @@ class EditCardViewController: UIViewController {
     var cardStatusReceived: Int?
     var cardTagsReceived: String?
     
+    var updatedCardStatus: Int?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -59,5 +61,60 @@ class EditCardViewController: UIViewController {
             cardStatusSwitch.setOn(false, animated: false)
         }
     }
-
+    
+    @IBAction func updateButtonTapped(_ sender: Any) {
+        
+        // Fetch Updated information from View
+        let updatedTitle = cardTitle.text
+        let updatedDescription = cardDescription.text
+        let updatedPricePerDay = pricePerDay.text
+        let updatedCategoryTags = cardCategoryTags.text
+        let updatedPlace = place.text
+        if (cardStatusSwitch.isOn) {
+            updatedCardStatus = 1
+        } else {
+            updatedCardStatus = 0
+        }
+        
+        // Check if any field(s) is/are empty
+        if (updatedTitle != "" && updatedDescription != "" && updatedPricePerDay != "" && updatedCategoryTags != "" && updatedPlace != "") {
+            
+            let param = ["id": cardIDReceived,
+                         "guide_id": guideIDReceived!,
+                         "card_title": updatedTitle!,
+                         "card_description": updatedDescription!,
+                         "price_per_day": updatedPricePerDay!,
+                         "place_ids": updatedPlace!,
+                         "service_status": serviceStatusReceived!,
+                         "card_status": updatedCardStatus!,
+                         "card_category_tags": updatedCategoryTags!,
+                         "api_key": API.API_key] as [String: Any]
+            
+            // Call updateCard by card ID api
+            Alamofire.request(API.baseURL + "/cards/updateCard", method: .post, parameters: param).validate().responseJSON {
+                response in
+                
+                if ((response.result.value) != nil) {
+                    do {
+                        //let getCardByGuideResponse = try JSONDecoder().decode(CardByGuide.self, from: response.data!)
+                        let editCardResponse = try JSONDecoder().decode(UpdateCard.self, from: response.data!)
+                        let message = editCardResponse.message
+                        self.displayAlertMessage("Card Updated", message)
+                        
+                    } catch {
+                        print("Error while parsing edit card json response")
+                    }
+                }
+            }
+        } else {
+            displayAlertMessage("Empty Fields!", "Fields must not be empty")
+        }
+    }
+    
+    func displayAlertMessage(_ title: String,_ userMessage: String) {
+        let userAlert = UIAlertController(title: title, message:  userMessage, preferredStyle: UIAlertController.Style.alert)
+        let okAction = UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: nil)
+        userAlert.addAction(okAction)
+        present(userAlert, animated: true, completion: nil)
+    }
 }
