fc6bb833172303b1c81875f06a7c9c2d2dbe2806
Sudipto Roy <sudiptoroy.cse@gmail.com>
2020-04-06 17:40:51 +0600

    Add show profile feature

--- TourPlanner/Controller/Tab Bar Controller/Profile Controller/ProfileViewController.swift
+++ TourPlanner/Controller/Tab Bar Controller/Profile Controller/ProfileViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Alamofire
 
 class ProfileViewController: UIViewController {
     
@@ -19,18 +20,38 @@ class ProfileViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("\nGuide ID in ProfileViewController: \(String(describing: guide_id))")
+        self.showProfile()
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func  showProfile() {
+        
+        let param = ["id" : guide_id!,
+                     "api_key": API.API_key] as [String: Any]
+        
+        Alamofire.request(API.baseURL + "/guides/ProfileSelf", method: .post, parameters: param).validate().responseJSON {
+            response in
+            
+            do {
+                let guideProfileResponse = try JSONDecoder().decode(GuideProfile.self, from: response.data!)
+                let firstName = guideProfileResponse.data[0].first_name
+                let lastName = guideProfileResponse.data[0].last_name
+                let rating = guideProfileResponse.data[0].ratings
+                let gender = guideProfileResponse.data[0].gender
+                
+                if (gender == "male") {
+                    self.profileImageView.image = UIImage(named: "ProfileMale")
+                } else if (gender == "female") {
+                    self.profileImageView.image = UIImage(named: "ProfileFemale")
+                }
+                
+                self.profileName.text = firstName! + " " + lastName!
+                self.rating.text = String(rating ?? 0.0)
+                
+                
+            } catch {
+                print("Error while parsing profile json data. Check it again")
+            }
+        }
     }
-    */
-
 }
