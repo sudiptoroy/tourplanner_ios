030a391a775897ec0f59d9f4039efa83d9291904
Sudipto Roy <sudiptoroy.cse@gmail.com>
2020-04-06 17:11:53 +0600

    Add basic profile ui and profile static images

---
+++ TourPlanner/Controller/Tab Bar Controller/Profile Controller/ProfileNavigationController.swift
@@ -0,0 +1,19 @@
+//
+//  ProfileNavigationController.swift
+//  TourPlanner
+//
+//  Created by Sudipto Roy on 4/6/20.
+//  Copyright © 2020 Code_x. All rights reserved.
+//
+
+import UIKit
+
+class ProfileNavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
---
+++ TourPlanner/Controller/Tab Bar Controller/Profile Controller/ProfileViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ProfileViewController.swift
+//  TourPlanner
+//
+//  Created by Sudipto Roy on 4/6/20.
+//  Copyright © 2020 Code_x. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var profileName: UILabel!
+    @IBOutlet weak var rating: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
