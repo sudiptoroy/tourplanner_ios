2a393d872404138394b2fc95c86cf713c030c8f7
Sudipto Roy <sudiptoroy.cse@gmail.com>
2020-04-08 13:34:08 +0600

    Prepare data to show in tableview (Ongoing service - in progress)

---
+++ TourPlanner/Controller/Tab Bar Controller/MyTour Controller/MyTourTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  MyTourTableViewCell.swift
+//  TourPlanner
+//
+//  Created by Sudipto Roy on 4/8/20.
+//  Copyright © 2020 Code_x. All rights reserved.
+//
+
+import UIKit
+
+class MyTourTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var cardImage: UIImageView!
+    @IBOutlet weak var cardTitle: UILabel!
+    @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var price: UILabel!
+    
+}
--- TourPlanner/Controller/Tab Bar Controller/MyTour Controller/MyTourViewController.swift
+++ TourPlanner/Controller/Tab Bar Controller/MyTour Controller/MyTourViewController.swift
@@ -9,16 +9,32 @@
 import UIKit
 import Alamofire
 
-class MyTourViewController: UIViewController {
+class MyTourViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
+    @IBOutlet weak var myTourTableView: UITableView!
     var guide_id: Int?
     var check = false
+    
+    var id = [String]()
+    var cardID = [String]()
+    var cardTitle = [String]()
+    var cardPrice = [String]()
+    var touristsID = [String]()
+    var isAccepted = [String]()
+    var isComplited = [String]()
+    var isCancelledByTourist = [String]()
+    var isCancelledByGuide = [String]()
+    var createdAt = [String]()
+    var updatedAt = [String]()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         print("\n\n========= My Tour =============")
         print("\n Guide ID in My tour View: \(String(describing: guide_id))")
         self.getTourGuideRelationByGuideID()
+        myTourTableView.delegate = self
+        myTourTableView.dataSource = self
     }
     
     
@@ -40,19 +56,72 @@ class MyTourViewController: UIViewController {
                 print("\ncheck: \(self.check)")
             }
             
-            if (self.check == true) {
+            if (self.check) {
                 do {
-                    // let getCardByGuideResponse = try JSONDecoder().decode(CardByGuide.self, from: response.data!)
                     let getTouristGuideRelation = try JSONDecoder().decode(TouristGuideRelation.self, from: response.data!)
-                    
+            
                     for relation in getTouristGuideRelation.data {
-                        print(relation.is_accepted!)
+                        print(relation.card_id!)
+                        self.cardID.append(String(relation.card_id!))
+                        self.id.append(String(relation.id!))
+                        print("Print ID count \(self.id.count)")
+                        self.isAccepted.append(String(relation.is_accepted!))
+                        self.isComplited.append(String(relation.is_complited!))
+                        self.isCancelledByGuide.append(String(relation.is_cancelled_by_guide!))
+                        self.isCancelledByTourist.append(String(relation.is_cancelled_by_tourist!))
+                        self.createdAt.append(relation.created_at!)
+                        self.updatedAt.append(relation.updated_at!)
+                        
+                        
+                        
+                        let param = ["id": relation.card_id!,
+                                     "api_key": API.API_key] as [String : Any]
+                        
+                        Alamofire.request(API.baseURL + "/cards/ByID", method: .post, parameters: param).validate().responseJSON {
+                            response in
+                            
+                            do {
+                                let getCardDetails = try JSONDecoder().decode(CardByID.self, from: response.data!)
+                                //print("Card Title = \(String(describing: getCardDetails.data[0].card_title))")
+                                self.cardTitle.append(getCardDetails.data[0].card_title!)
+                                self.cardPrice.append(String(getCardDetails.data[0].price_per_day!))
+                            } catch {
+                                print("Error while parsing json of card api response")
+                            }
+                        }
+                        if (self.id.count > 0) {
+                            self.myTourTableView!.reloadData()
+                        }
                     }
-                    
                 } catch {
                     print("Error while parsing json of Tourist Guide relation api response")
                 }
             }
         }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return id.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyTourTableViewCell", for: indexPath) as! MyTourTableViewCell
+        
+        cell.cardImage.image = UIImage(named: "StaticImage2")
+        cell.cardTitle.text = cardTitle[indexPath.item]
+        cell.price.text = cardPrice[indexPath.item]
+        
+        if (isAccepted[indexPath.item] == "0" && isComplited[indexPath.item] == "0") {
+            cell.statusLabel.text = "requested"
+        } else if (isAccepted[indexPath.item] == "1" && isComplited[indexPath.item] == "0") {
+            cell.statusLabel.text = "On Going"
+        } else if (isComplited[indexPath.item] == "1") {
+            cell.statusLabel.text = "Completed"
+        } else if (isCancelledByTourist[indexPath.item] == "1" || isCancelledByGuide[indexPath.item] == "1") {
+            cell.statusLabel.text = "Cancelled"
+        }
+        
+        return cell
     }
 }
