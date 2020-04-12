a475d2c19f05030fc37f10d4a686edcef02d75ca
Sudipto Roy <sudiptoroy.cse@gmail.com>
2020-04-12 18:12:44 +0600

    Add basic ui and controller for Tourist Profile view

---
+++ TourPlanner/Controller/Tab Bar Controller/MyTour Controller/TouristProfileViewController.swift
@@ -0,0 +1,24 @@
+//
+//  TouristProfileViewController.swift
+//  TourPlanner
+//
+//  Created by Sudipto Roy on 4/12/20.
+//  Copyright © 2020 Code_x. All rights reserved.
+//
+
+import UIKit
+
+class TouristProfileViewController: UIViewController {
+
+    @IBOutlet weak var coverImage: UIImageView!
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var touristName: UILabel!
+    @IBOutlet weak var country: UILabel!
+    @IBOutlet weak var gender: UILabel!
+    @IBOutlet weak var joinedDate: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
