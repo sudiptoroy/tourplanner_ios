afb4d9abdcb5959c719b0262189e196a07a2cc76
Sudipto Roy <sudiptoroy.cse@gmail.com>
2020-04-13 16:55:41 +0600

    Show tourist profile (done)

--- TourPlanner/Controller/Tab Bar Controller/MyTour Controller/TouristProfileViewController.swift
+++ TourPlanner/Controller/Tab Bar Controller/MyTour Controller/TouristProfileViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Alamofire
 
 class TouristProfileViewController: UIViewController {
 
@@ -23,5 +24,37 @@ class TouristProfileViewController: UIViewController {
         super.viewDidLoad()
         print("\n============= Touist Profile View ===============")
         print("Tourist Id Received: \(touristIDReceived)")
+        self.showTouristProfile()
+    }
+    
+    func showTouristProfile () {
+        
+        let param = ["id": touristIDReceived,
+                     "api_key": API.API_key] as [String: Any]
+        
+        Alamofire.request(API.baseURL + "/tourist/Profile", method: .post, parameters: param).validate().responseJSON {
+            response in
+            
+            print(response)
+            
+            do {
+                let getTouristProfile = try JSONDecoder().decode(TouristProfile.self, from: response.data!)
+                self.coverImage.image = UIImage(named: "StaticImage")
+                if (getTouristProfile.data[0].gender == "Male") {
+                    self.profileImage.image = UIImage(named: "ProfileMale")
+                } else if (getTouristProfile.data[0].gender == "Female") {
+                    self.profileImage.image = UIImage(named: "ProfileFemale")
+                } else {
+                    self.profileImage.image = UIImage(named: "ProfileMale")
+                }
+                
+                self.touristName.text = (getTouristProfile.data[0].first_name ?? "Not") + " " + (getTouristProfile.data[0].last_name ?? "Found")
+                self.country.text = getTouristProfile.data[0].country ?? "Not Found"
+                self.gender.text = getTouristProfile.data[0].gender ?? "Not Found"
+                self.dateOfBirth.text = getTouristProfile.data[0].date_of_birth ?? "Not Found"
+            } catch {
+                print("Error while parsing tourist profile json data")
+            }
+        }
     }
 }
