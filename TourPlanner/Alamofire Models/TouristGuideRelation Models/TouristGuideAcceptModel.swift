4570560f5b279e780aab60b0261dfc4817605835
Sudipto Roy <sudiptoroy.cse@gmail.com>
2020-04-15 17:03:54 +0600

    Add tourist guide relation accept model

---
+++ TourPlanner/Alamofire Models/TouristGuideRelation Models/TouristGuideAcceptModel.swift
@@ -0,0 +1,14 @@
+//
+//  TouristGuideAcceptModel.swift
+//  TourPlanner
+//
+//  Created by Sudipto Roy on 4/15/20.
+//  Copyright © 2020 Code_x. All rights reserved.
+//
+
+import Foundation
+
+struct TouristGuideAccept: Decodable {
+    let success: Bool?
+    let message: String?
+}
